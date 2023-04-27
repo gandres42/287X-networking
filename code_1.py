807b4545a6150fb43c4bff40e3f0cae24ed14c82
gandres2416 <gandres2416@gmail.com>
2023-04-26 23:25:51 -0500

    an actually reliable heartbeat and socket wrapper
    what if the IASTATE network was the friends we made along the way?

--- code_1.py
+++ code_1.py
@@ -11,11 +11,16 @@ SERVER_IP = "192.168.70.130"
 BUFSIZE = 64
 NODE_TYPE = 0
 
+PROTAGONIST_TIMEOUT = 1
+ANTAGONOST_TIMEOUT = 2
+
 pool = socketpool.SocketPool(wifi.radio)
 socket = None
 clients = {}
 client_buf = bytearray([0] * BUFSIZE)
 client_buf_pointer = 0
+client_last_revc = 0
+client_last_send = 0
 pending_msgs = ""
 
 def connect_wifi():
@@ -51,7 +56,9 @@ def accept_clients():
                 'buf': bytearray([0] * BUFSIZE),
                 'addr': addr,
                 'buf_pointer': 0,
-                'pend_msg': ""
+                'pend_msg': "",
+                'last_recv': time.monotonic(),
+                'last_send': time.monotonic()
             }
     except Exception as e:
         pass
@@ -112,11 +119,27 @@ def client_send(msg):
         # socket busy or connection is dead
         pass
 
+def client_heartbeat():
+    global client_last_send
+    if time.monotonic() - client_last_send >= 1:
+        client_send("keepalive")
+
+def server_heartbeat():
+    global clients
+    for client in clients.values():
+        if time.monotonic() - client['last_send'] >= 1:
+            try:
+                client['sock'].send(b'keepalive')
+            except:
+                pass
+            client['last_send'] = time.monotonic()
+
 def client_recv():
-    global socket, pool, client_buf, client_buf_pointer
+    global socket, pool, client_buf, client_buf_pointer, client_last_revc
     try:
         i = socket.recv_into(memoryview(client_buf)[client_buf_pointer:], BUFSIZE - client_buf_pointer)
         if i == 0:
+            print("server disconnected")
             socket.close()
             socket = None
         else:
@@ -124,37 +147,58 @@ def client_recv():
             if client_buf_pointer == BUFSIZE:
                 client_buf_pointer = 0
             print(client_buf)
+            client_last_revc = time.monotonic()
     except Exception as e:
         pass
 
 def new_connection(host, port, timeout):
-    global socket, pool
+    global socket, pool, client_last_revc, client_last_send
     socket = pool.socket(pool.AF_INET, pool.SOCK_STREAM)
     socket.settimeout(timeout)
     socket.setblocking(False)
     try:
         socket.connect((host, port))
+        client_last_revc = time.monotonic()
+        client_last_send = time.monotonic()
         print("connected to: " + str(host) + " " + str())
     except:
         print("Connection failed")
         socket.close()
         socket = None
 
+def prune_clients():
+    global clients
+    for client in clients.values():
+        if time.monotonic() - client['last_recv'] > 2:
+            print("client at " + str(client['addr']) + " time out")
+            client['sock'].close()
+            clients.pop(client['addr'])
+
+def prune_server():
+    global client_last_revc, socket
+    if time.monotonic() - client_last_revc > 2:
+        print("server timed out")
+        socket.close()
+        socket = None
+
 def loop():
     global socket, pool
+    # server
     if NODE_TYPE == 1:
         if socket == None:
             start_accepting('0.0.0.0', 8080, None)
         accept_clients()
         server_recv()
-        server_broadcast("hello")
+        server_heartbeat()
+        prune_clients()
+        # client
     if NODE_TYPE == 2:
         if socket == None:
             new_connection(SERVER_IP, 8080, None)
         else:
             client_recv()
-            client_send("hello")
-
+            client_heartbeat()
+            prune_server()
 
 connect_wifi()
 random.seed(time.monotonic_ns())
